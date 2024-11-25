092b00a21b0c353d608f0d1042b650522ae88d64
Mauricio Martinez <mauromm1603@gmail.com>
2024-11-25 13:48:13 -0500

    12312

--- routes/restaurantRoute.js
+++ routes/restaurantRoute.js
@@ -1,15 +1,19 @@
 import {Router} from 'express';
 import {
-    getRestaurants//,
-    //getUser,
-    //registerUser,
-    //updateUser,
-    //loginUser
+    getRestaurants,
+    getRestaurant,
+    updateRestaurant,
+    deleteRestaurante,
+    registerRestaurant
 } from '../controllers/restaurantController.js';
 
 const restaurantRouter = Router ();
 
 restaurantRouter.get('/getRestaurants', getRestaurants);
+restaurantRouter.get('/getRestaurant:', getRestaurant);
+restaurantRouter.put('/updateRestaurant', updateRestaurant);
+restaurantRouter.delete('/deleteRestaurant,', deleteRestaurante);
+restaurantRouter.post('/registerRestaurant', registerRestaurant);
 
 
 export default restaurantRouter;
